78c5f5a8a635091a5de87d25c29563a99a36f27b
vskelton <vskelton25@gmail.com>
2024-09-26 12:13:00 -0400

    Updated code to look cleaner

--- index.js
+++ index.js
@@ -1,55 +1,27 @@
 const express = require('express'),
-    morgan = require('morgan');
+constmorgan = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
-    const app = express();
-    const bodyParser = require('body-parser'),
-    methodOverride = require('method-override');
+const app = express();
 
     // Morgan middleware
     app.use(morgan('combined'));
 
+// Static request
+app.use(express.static('public'));
 
 let topMovies = [
-    {
-        title: 'Top Gun',
-        year: '1986'
-    },
-    {
-        title: 'Jurassic Park',
-        year: '1993',
-    },
-    {
-        title: 'Pulp Ficton',
-        year: '1994'
-    },
-    {
-        title: 'Trap',
-        year: '2024'
-    },
-    {
-        title: 'Bad Boys',
-        year: '1995'
-    },
-    {
-        title: 'Trolls',
-        year: '2016'
-    },
-    {
-        title: 'Black Panther',
-        year: '2018'
-    },
-    {
-        title: 'The Sandlot',
-        year: '1993'
-    },
-    {
-        title: 'The Lion King',
-        year: '1994'
-    },
-    {
-        title: 'Split',
-        year: 2016
-    }
+    { title: 'Top Gun', year: '1986' },
+    { title: 'Jurassic Park', year: '1993', },
+    { title: 'Pulp Ficton', year: '1994' },
+    { title: 'Trap', year: '2024' },
+    { title: 'Bad Boys', year: '1995' },
+    { title: 'Trolls', year: '2016' },
+    { title: 'Black Panther', year: '2018' },
+    { title: 'The Sandlot', year: '1993' },
+    { title: 'The Lion King', year: '1994' },
+    { title: 'Split', year: 2016 }
 ];
 
 // Get requests
@@ -58,15 +30,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/documentation', (req, res) => {
-    res.sendFile('public/documentation.html', { root: __dirname});
+    res.sendFile('public/documentation.html', { root: 'public'});
 });
 
 app.get('/movies', (req,res) => {
     res.json(topMovies);
 });
 
-// Static request
-app.use(express.static('public'));
+
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -79,6 +50,7 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke');
 });
+
 //listen for request
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
