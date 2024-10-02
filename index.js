0cc36336d157bcf0e511e38845ae55b041c90f13
vskelton <vskelton25@gmail.com>
2024-10-02 11:51:40 -0400

    push

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express'),
-constmorgan = require('morgan');
+const express = require('express');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
+const uuid = require('uuid');
 
 const app = express();
+
+app.use(bodyParser.json());
 
     // Morgan middleware
     app.use(morgan('combined'));
@@ -11,46 +13,159 @@ const app = express();
 // Static request
 app.use(express.static('public'));
 
-let topMovies = [
-    { title: 'Top Gun', year: '1986' },
-    { title: 'Jurassic Park', year: '1993', },
-    { title: 'Pulp Ficton', year: '1994' },
-    { title: 'Trap', year: '2024' },
-    { title: 'Bad Boys', year: '1995' },
-    { title: 'Trolls', year: '2016' },
-    { title: 'Black Panther', year: '2018' },
-    { title: 'The Sandlot', year: '1993' },
-    { title: 'The Lion King', year: '1994' },
-    { title: 'Split', year: 2016 }
+let movies = [
+    { 
+        "Title": 'Top Gun', 
+        "Year": '1986',
+
+      "Director": {
+        "Name": 'Tony Scott'},
+
+      "Genre": {
+        "Name": 'Drama'},
+      },
+
+    { 
+        "Title": 'Jurassic Park',
+        "Year": '1993',
+
+      "Director": {
+        "Name": 'Steven Spielberg'},
+
+      "Genre":{
+        "Name": 'Science Fiction' },
+      },
+
+    { 
+        "Title": 'Pulp Ficton', 
+        "Year": '1994',
+
+      "Director": {
+        "Name": 'Quentin Tarantino'},
+
+      "Genre": {
+        "Name": 'Drama' },
+      }
 ];
 
-// Get requests
-app.get('/', (req, res) => {
-    res.send('Welcome to my movie app!');
-});
+let users = [
+    {
+        "id": 1,
+        "name": 'Jayden Smith',
+        "favoriteMovies": ['Jurassic Park']
+    },
+    {
+        "id": 2,
+        "name": 'Destiny Johnson',
+        "favoriteMovies": ['Top Gun']
+    },
+];
 
-app.get('/documentation', (req, res) => {
-    res.sendFile('public/documentation.html', { root: 'public'});
-});
+// CREATE
+app.post('/users', (req, res) => {
+    const newUser = req.body;
 
-app.get('/movies', (req,res) => {
-    res.json(topMovies);
-});
+    if (newUser.name) {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).json(newUser)
+    } else {
+        res.status(400).send('users need names')
+    }
+})
 
+// UPDATE
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedUser = req.body;
 
+    let user = users.find( user => user.id == id);
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+    if (user) {
+        user.name = updatedUser.name;
+        res.status(200.).json(user);
+    } else {
+        res.status(400).send('no such user')
+    }
+})
 
-app.use(bodyParser.json());
-app.use(methodOverride());
+app.post('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke');
-});
+    let user = users.find( user => user.id == id);
 
+    if (user) {
+        user.favoriteMovies.push(movieTitle);
+        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+    } else {
+        res.status(400).send('no such user')
+    }
+})
+
+// READ
+app.get('/movies', (req, res) => {
+    res.status(200).json(movies);
+})
+
+app.get('/movies/:title', (req, res) => {
+    const { title } = req.params;
+    const movie = movies.find( movie => movie.Title === title);
+
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(400).send('no such movie')
+    }
+})
+
+app.get('/movies/genre/:genreName', (req, res) => {
+    const { genreName } = req.params;
+    const genre = movies.find( movie => movie.Genre.Name === genreName).Genre;
+
+    if (genre) {
+        res.status(200).json(genre);
+    } else {
+        res.status(400).send('no such gnere')
+    }
+})
+
+app.get('/movies/directors/:directorName', (req, res) => {
+    const { directorName } = req.params;
+    const director = movies.find( movie => movie.Director.Name === directorName).Director;
+
+    if (director) {
+        res.status(200).json(director);
+    } else {
+        res.status(400).send('no such director')
+    }
+})
+
+// DELETE
+app.delete('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find( user => user.id == id );
+
+    if (user) {
+        user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+        res.status(200).send(`${movieTitle} has been deleted from ${id}'s array`);
+    } else {
+        res.status(400).send('no such user')
+    }
+})
+
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    let user = users.find( user => user.id == id);
+
+    if (user) {
+        users = users.filter( user => user.id != id);
+        res.status(200).send(`user ${id}'s has been deleted`);
+    } else {
+        res.status(400).send('no such user')
+    }
+})
 //listen for request
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
