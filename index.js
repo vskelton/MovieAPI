b7a84599773d06b226a48e70fa5bd289f661c61c
vskelton <vskelton25@gmail.com>
2024-10-17 13:19:47 -0400

    modified index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 // mongoose.connect('mongodb://127.0.0.1:27017/movieapp', { useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
