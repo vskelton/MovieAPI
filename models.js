fc0c20e0771ddc7c7780d552133196ca7075e7f4
vskelton <vskelton25@gmail.com>
2024-10-18 11:10:38 -0400

    spell check error

--- models.js
+++ models.js
@@ -30,7 +30,7 @@ userSchema.statics.hashPassword = (password) => {
 };
 
 userSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSynce(password, this.Password);
+    return bcrypt.compareSync(password, this.Password);
 };
 
 let Movie = mongoose.model('Movie', movieSchema);
