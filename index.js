3021813c7e50e5a6ea0682ddb74afc2bc0f11102
vskelton <vskelton25@gmail.com>
2024-11-07 12:37:18 -0500

    added get request for username

--- index.js
+++ index.js
@@ -119,6 +119,19 @@ app.get('/',(req, res) => {
     res.send('Welcome to my Movie App!');
 });
 
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+      const user = await Users.findOne({ Username: req.params.Username });
+      if (!user) {
+          return res.status(404).send('User not found');
+      }
+      res.json(user); // Send the user data as a JSON response
+  } catch (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+  }
+});
+
 app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Movies.find()
         .then((movies) => {
